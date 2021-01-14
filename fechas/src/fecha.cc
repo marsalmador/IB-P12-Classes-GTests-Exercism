d5254b7b3e6b680b26439e11f85f9447b831cfa7
Marlon Eduardo <alu0101433943@ull.edu.es>
2021-01-14 16:05:46 +0000

    Update fecha.cc

--- fechas/src/fecha.cc
+++ fechas/src/fecha.cc
@@ -5,17 +5,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**Pre: anyo >= 1600
- *Post: Devuelve <true> si y solo si <anyo> es un año
- *bisiesto
+/**
+ * Every year that is exactly divisible by four is a leap year, except for
+ * years that are exactly divisible by 100, but these centurial years are
+ * leap years if they are exactly divisible by 400. That's what the
+ * function checks.
  */
 bool esBisiesto (const Fecha &fecha){
- //Un año es bisiesto si es múltiplo de 400 o, siendo
- //múltiplo de 4, no lo es de 100. En caso contrario
- //no es bisiesto
+
  return(fecha.ano % 4 == 0 && fecha.ano % 100 != 0)||(fecha.ano % 400 == 0);
 }
 
+/**
+ * This function gets each part of the dates given and compares them with
+ * logical operators.
+ */
 bool comparar(const Fecha &fecha1, const Fecha &fecha2){
   if(fecha1.ano < fecha2.ano){
     return true;
@@ -28,6 +32,13 @@ bool comparar(const Fecha &fecha1, const Fecha &fecha2){
   }
   return false;
 }
+
+/**
+ * By using "sort" we sort the dates in the array given. We use "arr+n"
+ * because we need to give the beginning and the end of the array to
+ * sort that interval, and "n" is the total number of elements the
+ * array has.
+ */
 void ordenarFechas(Fecha arr[], int n){
   std::sort(arr, arr+n, comparar);
 }
